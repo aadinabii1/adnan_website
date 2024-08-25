93bcdca6981c751eddfd043d89be83e72a4fe12b
aadinabii1 <aadinabii1@gmail.com>
2024-08-25 12:21:02 +0530

    Adde user managment function in user_mgt.py

---
+++ user_mgt.py
@@ -0,0 +1,6 @@
+def add user(username):
+   print(f"User (username) added successfully!")
+def delete user(username):
+   print(f"User {username) deleted successfully!")
+add user("john doe")
+delete user ("john doe")
